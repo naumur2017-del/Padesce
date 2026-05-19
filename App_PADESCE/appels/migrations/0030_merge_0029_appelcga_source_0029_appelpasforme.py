51553087bf5d1ed3a657cfccff4eb0ffc3afa2c2
naumur2017-del <naumur2017@gmail.com>
2026-05-19 13:59:55 +0100

    Merge appels migration heads

---
+++ App_PADESCE/appels/migrations/0030_merge_0029_appelcga_source_0029_appelpasforme.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("appels", "0029_appelcga_source"),
+        ("appels", "0029_appelpasforme"),
+    ]
+
+    operations = []
